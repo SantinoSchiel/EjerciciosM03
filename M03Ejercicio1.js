09c1151e9e43995d91755220b4b2880cf2adf571
SantinoSchiel <143773471+SantinoSchiel@users.noreply.github.com>
2023-09-02 20:09:01 -0300

    EjerciciosM03

---
+++ M03Ejercicio1.js
@@ -0,0 +1,43 @@
+/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
+/*1️⃣ EJERCICIO 01 1️⃣*/
+
+// En los siguientes ejercicios deberás reemplazar el valor null
+// por el correspondiente.
+
+// Crea una variable de tipo string.
+const nuevoString = "VehiculoMotorizado";
+
+// Crea una variable de tipo number.
+const nuevoNumero = 7;
+const nuevoNumero1 = 4;
+const nuevoNumero2 = 1;
+
+// Crea una variable de tipo boolean.
+const nuevoBoolean = false;
+
+// Resuelve el siguiente problema matemático.
+const nuevaResta = 10 - nuevoNumero === 3;
+
+// Resuelve el siguiente problema matemático.
+const nuevaMultiplicacion = 10 * nuevoNumero1 === 40;
+
+// Resuelve el siguiente problema matemático.
+const nuevoModulo = 21 % 5 === nuevoNumero2;
+
+/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+module.exports = {
+   nuevoString,
+   nuevoNumero,
+   nuevoBoolean,
+   nuevaResta,
+   nuevaMultiplicacion,
+   nuevoModulo,
+};
+
+console.log(nuevoNumero);
+console.log(nuevoNumero1);
+console.log(nuevoNumero2);
+console.log(nuevoBoolean);
+console.log(nuevaResta);
+console.log(nuevaMultiplicacion);
+console.log(nuevoModulo);
